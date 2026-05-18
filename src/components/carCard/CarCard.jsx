ab7e12cebbcf675b000f4b9d5025afcd1fb59eee
Guilherme Moreira <guilherme.moreira1108@gmail.com>
2026-05-17 21:08:30 -0300

    feat: tela home integrada

--- src/components/carCard/CarCard.jsx
+++ src/components/carCard/CarCard.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "../../pages/home/Home.css";
 
-const CarCard = ({ car, isDesktop, onNavigate }) => {
+const CarCard = ({ car, onNavigate, scoreClass }) => {
+
   return (
+    
     <div
       className="card"
-      onClick={ () => onNavigate("./veiculo")}
+      onClick={() => onNavigate("./veiculo")}
     >
 
       <img className="imgCar" src={car.image} alt={car.name} />
@@ -14,7 +16,7 @@ const CarCard = ({ car, isDesktop, onNavigate }) => {
 
       <div className="lowerInformations">
         <span className="carPlate">{car.plate}</span>
-        <span className="carPoints">{car.points}</span>
+        <span className={`carPoints ${scoreClass}`}>{car.points}</span>
       </div>
 
       <div className="alinhamentoBtn">
