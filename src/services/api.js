a91a69e620fb3b7d399b49aceaf60b45f4f3d141
Guilherme Moreira <guilherme.moreira1108@gmail.com>
2026-06-24 10:08:38 -0300

    fix: voltando para a url da azure

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/v1/car-assist'
+  baseURL: 'https://carassist-backend-f3h6b8b7bjeshrhr.brazilsouth-01.azurewebsites.net/v1/car-assist'
 });
 
 export default api;
